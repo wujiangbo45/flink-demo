acad278e4b7d5361f2ab65fe0e17b3ce7e49804c
wujiangbo <285874318@qq.com>
2020-03-09 17:27:01 +0800

    first commit

---
+++ src/main/java/com/navinfo/opentsp/flink/streaming/WorldCount.java
@@ -0,0 +1,42 @@
+package com.navinfo.opentsp.flink.streaming;
+
+import akka.stream.impl.CollectorState;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+public class WorldCount {
+
+    // use nc -lk 9999 to start tcp server
+
+    /**
+     * word count
+     * 单词统计
+     * 对输入的单词计数(统计每5秒内输入的单词数)
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .socketTextStream("localhost", 9999)
+                .flatMap((String word, Collector<Tuple2<String,Integer>> out) -> {
+                    for (String s : word.split(" ")) {
+                        out.collect(new Tuple2<>(s, 1));
+                    }
+                })
+                .returns(Types.TUPLE(Types.STRING,Types.INT))
+                .keyBy(1)
+                .timeWindow(Time.seconds(5))
+                .sum(1).name("word count main ");
+        dataStream.print();
+        env.execute("Window WordCount");
+    }
+
+
+}
